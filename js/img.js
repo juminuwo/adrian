02fffa2146c76bb276d92fa6709d14fc8e8e2216
juminuwo <howismypielola@gmail.com>
2017-08-22 21:30:33 +0100

    first

---
+++ js/img.js
@@ -0,0 +1,11 @@
+var imageURLs = [
+        "../img/1.png", "../img/2.png", "../img/3.png", "../img/4.png"
+    ];
+function getImageTag() {
+    "use strict";
+    var img = '<img src=\"',
+        randomIndex = Math.floor(Math.random() * imageURLs.length);
+    img += imageURLs[randomIndex];
+    img += '\" alt=\"Some cool pics ;)\"/>';
+    return img;
+}
