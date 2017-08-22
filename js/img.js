c698deb0e70f085fb0c3847443ee611f67ebef5d
juminuwo <howismypielola@gmail.com>
2017-08-22 21:39:51 +0100

    change

--- js/img.js
+++ js/img.js
@@ -1,5 +1,5 @@
 var imageURLs = [
-        "../img/1.png", "../img/2.png", "../img/3.png", "../img/4.png"
+        "img/1.png", "img/2.png", "img/3.png", "img/4.png"
     ];
 function getImageTag() {
     "use strict";
--- test.js
+++
@@ -1,7 +0,0 @@
-window.onload = choosePic;
-
-var myPix = new Array("img/2.png","img/3.jpg","img/4.png");
-
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("myPicture").src = myPix[randomNum];
