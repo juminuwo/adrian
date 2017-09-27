8afe8c69b4b24c2e7f0d07e8b36dd44c373d5efb
juminuwo <howismypielola@gmail.com>
2017-09-27 14:15:01 +0100

    index page in pandoc

--- pandoc/create.sh
+++ pandoc/create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo Hello, this is a script to convert markdown files to html with a pretty css I copied from someguys GitHub.
-echo Please input location of .md file.
+echo Please input location/name of the markdown file. (Exclude file extension)
 read loc
-echo Input name for the html document.
+echo Input location/name for the html document. (Exclude file extension)
 read name
 
-pandoc $loc --css pandoc.css -o $name.html
+pandoc $loc.md --css pandoc.css -o $name.html
